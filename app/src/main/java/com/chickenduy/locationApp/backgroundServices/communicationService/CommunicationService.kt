c4d55b919e854d252a6dafef787a04b65118e557
TheChickenDuy <khanhduydinhle@gmail.com>
2019-12-16 12:40:58 +0900

    remove FCM

--- app/src/main/java/com/chickenduy/locationApp/backgroundServices/communicationService/CommunicationService.kt
+++ app/src/main/java/com/chickenduy/locationApp/backgroundServices/communicationService/CommunicationService.kt
@@ -1,24 +1,6 @@
 package com.chickenduy.locationApp.backgroundServices.communicationService
 
-import android.content.Context
-import android.util.Log
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
-
-class CommunicationService: FirebaseMessagingService()  {
+class CommunicationService  {
     private val TAG = "COMSERVICE"
-
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-        Log.d(TAG, "From: ${remoteMessage.from}")
-        // Check if message contains a data payload.
-        if(remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
-        }
-    }
-
-    override fun onDeletedMessages() {
-        super.onDeletedMessages()
-    }
 
 }
